5885267e90bf885cff836c7cb63fd15b209e0712
Михаил Казаков <mishakazakov82@gmail.com>
2026-04-20 02:04:40 +0500

    Выполненная практическая работа на оценку удовлетворительно. Осенников

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Word_kazakov.Context;
 
 namespace Word_kazakov
 {
@@ -23,6 +25,22 @@ namespace Word_kazakov
         public MainWindow()
         {
             InitializeComponent();
+            LoadRooms();
+        }
+
+        private void Report(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "Word Files (*.docx)|*.docx";
+            sfd.ShowDialog();
+            if (sfd.FileName != "")
+                OwnerContext.Report(sfd.FileName);
+        }
+
+        public void LoadRooms()
+        {
+            for (int i = 1; i < 20; i++)
+                Parent.Children.Add(new Elements.Room(i));
         }
     }
 }
